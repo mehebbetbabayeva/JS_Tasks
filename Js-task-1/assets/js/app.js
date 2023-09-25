b4963cf9daafa7b3fcd77d807a4d78358193f814
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2023-09-25 20:12:06 +0400

    finish task 1 2 3

---
+++ Js-task-1/assets/js/app.js
@@ -0,0 +1,5 @@
+//area-of-rectangle
+let width=Number(prompt("düzbucaqlının uzunluğunu daxil edin"));
+let height=Number(prompt("düzbucaqlının hündürlüyünü daxil edin"));
+let result = width*height;
+alert("duzbucaqlinin sahesi "+result)
